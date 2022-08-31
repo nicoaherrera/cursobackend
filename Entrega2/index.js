5ce287f966625e6532c815172c4562b98551063a
Nicolas A. Herrera <Nicoaherrera@MacBook-Pro-de-Nicolas.local>
2022-08-31 20:36:33 -0300

    entrega completa

--- Entrega2/index.js
+++ Entrega2/index.js
@@ -7,33 +7,79 @@ class Contenedor {
     }
 
     async save (informacion) {
-        let id
+        
         try{
             let contenido = await fs.promises.readFile(`./${this.name}`,'utf-8')
-            let contenidojson = JSON.parse(contenido)
+            let contenidoJson = JSON.parse(contenido)
             let ultimoIndice = contenidojson.length - 1
-            let ultimoId = contenidojson[ultimoIndice].id
+            let ultimoId = contenidoJson[ultimoIndice].id
             informacion.id = ultimoId + 1
             let id = informacion.id
             contenidojson.push(informacion)
-            console.log (contenidojson)
+            await fs.promises.writeFile(`./${this.name}`,JSON.stringify(contenidojson)  ) 
+            return id         
+
+           // console.log (contenidojson)
         }
         catch (error){
             console.log(error)
         }
         return id
     }
-    getById (id){
+    async getById (id){
+        try{
+            let contenido = await fs.promises.readFile(`./${this.name}`,'utf-8')
+            let contenidoJson = JSON.parse(contenido)
+            let contenidoExtraido
 
+            contenidoJson.forEach(element => {
+                if(element.id == id){
+                    contenidoExtraido = element
+                }
+            });
+            return contenidoExtraido
+
+        }
+        catch (error){console.log(error)}
     }
-    getAll (){
-
+   async getAll (){
+        try {
+            let contenido = await fs.promises.readFile(`./${this.name}`,'utf-8')
+            let contenidoJson = JSON.parse(contenido)
+            return contenidoJson
+        }
+        catch (error){console.log(error)}
+        
     }
-    deleteById (id) {
+    async deleteById(idToDelete) {
+        try {
+            const arr = await this.getAll()
+            let match = arr.find(item => item.id === idToDelete)
+            if (match == undefined) {
+                return console.log(`No existe el producto‼`)
+            }
+            else {
+                let toDelete = arr.indexOf(match)
+                arr.splice(toDelete, 1)
+                await fs.promises.writeFile(this.fileName, JSON.stringify(arr))
+                return console.log(`Producto eliminado ❎`)
+            }
 
+        }
+        catch(err) {
+            console.log(err)
+        }
     }
-    deleteAll () {
 
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(this.fileName, JSON.stringify([]))
+            return console.log(`Todos los productos borrados!`)
+
+        }
+        catch(err) {
+            console.log(err)
+        }
     }
 
 }
@@ -45,4 +91,24 @@ let informacionNueva = {
          "price":"80"
 }
 
-contenedor.save (informacionNueva)
+
+
+/* contenedor.getById(2).then(resPromise => {
+    console.log(resPromise)
+})
+
+contenedor.save(informacionNueva).then(resPromise => {
+    console.log(resPromise)
+})
+
+contenedor.getAll().then(resPromise => {
+    console.log(resPromise)
+})
+
+contenedor.deleteById(3).then(resPromise => {
+    console.log(resPromise)
+})
+
+contenedor.deleteAll().then(resPromise => {
+    console.log(resPromise)
+}) */
